58557c9b1d4df0dcb7a31bf914c3fe75af9e0170
Mathew <su880303@gmail.com>
2024-04-05 22:31:49 +0800

    Replace __builtin_clz(x) function
    In order to make the program independent of GNU extensions, I change
    function __builtin_clz(x) to linux function __fls, which is to find last
    set bit in a long word.

--- sqrt/sqrt.c
+++ sqrt/sqrt.c
@@ -1,12 +1,52 @@
 #include <stdio.h>
 
+/* Assume 32-bit architecture */
+#define BITS_PER_LONG 32
+
+/**
+ * __fls - find last (most-significant) set bit in a long word
+ * @word: the word to search
+ *
+ * Undefined if no set bit exists, so code should check against 0 first.
+ */
+static inline unsigned long __fls(unsigned long word)
+{
+	int num = BITS_PER_LONG - 1;
+
+#if BITS_PER_LONG == 64
+	if (!(word & (~0ul << 32))) {
+		num -= 32;
+		word <<= 32;
+	}
+#endif
+	if (!(word & (~0ul << (BITS_PER_LONG-16)))) {
+		num -= 16;
+		word <<= 16;
+	}
+	if (!(word & (~0ul << (BITS_PER_LONG-8)))) {
+		num -= 8;
+		word <<= 8;
+	}
+	if (!(word & (~0ul << (BITS_PER_LONG-4)))) {
+		num -= 4;
+		word <<= 4;
+	}
+	if (!(word & (~0ul << (BITS_PER_LONG-2)))) {
+		num -= 2;
+		word <<= 2;
+	}
+	if (!(word & (~0ul << (BITS_PER_LONG-1))))
+		num -= 1;
+	return num;
+}
+
 int i_sqrt(int x)
 {
     if (x <= 1) /* Assume x is always positive */
         return x;
 
     int z = 0;
-    for (int m = 1UL << ((31 - __builtin_clz(x)) & ~1UL); m; m >>= 2) {
+    for (int m = 1UL << ((__fls(x)) & ~1UL); m; m >>= 2) {
         int b = z + m;
         z >>= 1;
         if (x >= b)
