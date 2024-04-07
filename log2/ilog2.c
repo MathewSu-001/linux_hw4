a1de6005a63199a29e3ab48d15d378846c67287f
Mathew <su880303@gmail.com>
2024-04-07 15:01:04 +0800

    Try another way for count log2
    Inspired by __builtin_clz, which function is return the number of
    leading 0-bits in x, I think __fls, which is to find last set bit in a
    long word, is similar with __builtin_clz. So I add function __fls in
    program, and try whether their outcome is the same.

--- log2/ilog2.c
+++ log2/ilog2.c
@@ -1,5 +1,45 @@
 #include <stdio.h>
 
+/* Assume 32-bit architecture */
+#define BITS_PER_LONG 32
+
+/**
+ * __fls - find last (most-significant) set bit in a long word
+ * @word: the word to search
+ *
+ * Undefined if no set bit exists, so code should check against 0 first.
+ */
+static inline unsigned long __fls(unsigned long word)
+{
+	int num = BITS_PER_LONG - 1;
+
+#if BITS_PER_LONG == 64
+	if (!(word & (~0ul << 32))) {
+		num -= 32;
+		word <<= 32;
+	}
+#endif
+	if (!(word & (~0ul << (BITS_PER_LONG-16)))) {
+		num -= 16;
+		word <<= 16;
+	}
+	if (!(word & (~0ul << (BITS_PER_LONG-8)))) {
+		num -= 8;
+		word <<= 8;
+	}
+	if (!(word & (~0ul << (BITS_PER_LONG-4)))) {
+		num -= 4;
+		word <<= 4;
+	}
+	if (!(word & (~0ul << (BITS_PER_LONG-2)))) {
+		num -= 2;
+		word <<= 2;
+	}
+	if (!(word & (~0ul << (BITS_PER_LONG-1))))
+		num -= 1;
+	return num;
+}
+
 static size_t ilog2(size_t i)
 {
     size_t result = 0;
@@ -32,5 +72,6 @@ int main()
     int N = 256;
     printf("%lu\n", ilog2(N));
     printf("%d\n", ilog32(N));
+    printf("%lu\n", __fls(N));
     return 0;
 }
